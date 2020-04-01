4991a149c26a175c944209bd8310946338908abd
Cook Green <dell20091234@gmail.com>
2020-04-02 07:59:07 +0800

    Update Project

--- ExampleMod/MyModule.cs
+++ ExampleMod/MyModule.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Taleworlds.Core
 
 namespace ExampleMod
 {
-    public class MyModule
+    public class MyModule : MBModuleBase
     {
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# MBBannerlordModTemplate
+# M&B Bannerlord Mod Template
 Mod Template for Mount & Blade Bannerlord Mods
+
+## Usage
+Replace the mod.config variable with your own:
+* BANNERLORD_DIRECTORY
+* MOD_NAME
+* MOD_FOLDER
+* MOD_ASSEMBLY_NAME
