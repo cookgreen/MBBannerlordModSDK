425eee35f6733d252c79872c46ef76507683ee08
Cook Green <dell20091234@gmail.com>
2020-04-08 15:04:25 +0800

    Cleanup the code base

--- make.ps1
+++ make.ps1
@@ -64,55 +64,6 @@ function All-Command
 	}
 }
 
-function Clean-Command
-{
-	If (!(Test-Path "*.sln"))
-	{
-		return
-	}
-
-	if ((CheckForDotnet) -eq 1)
-	{
-		return
-	}
-
-	dotnet clean /nologo
-	rm *.dll
-	rm mods/*/*.dll
-	rm *.dll.config
-	rm *.pdb
-	rm mods/*/*.pdb
-	rm *.exe
-	rm ./*/bin -r
-	rm ./*/obj -r
-
-	rm $env:ENGINE_DIRECTORY/*.dll
-	rm $env:ENGINE_DIRECTORY/mods/*/*.dll
-	rm env:ENGINE_DIRECTORY/*.config
-	rm env:ENGINE_DIRECTORY/*.pdb
-	rm mods/*/*.pdb
-	rm env:ENGINE_DIRECTORY/*.exe
-	rm env:ENGINE_DIRECTORY/*/bin -r
-	rm env:ENGINE_DIRECTORY/*/obj -r
-	if (Test-Path env:ENGINE_DIRECTORY/thirdparty/download/)
-	{
-		rmdir env:ENGINE_DIRECTORY/thirdparty/download -Recurse -Force
-	}
-
-	Write-Host "Clean complete." -ForegroundColor Green
-}
-
-function CheckForUtility
-{
-	if (Test-Path $utilityPath)
-	{
-		return 0
-	}
-
-	Write-Host "OpenRA.Utility.exe could not be found. Build the project first using the `"all`" command." -ForegroundColor Red
-	return 1
-}
-
 function CheckForDotnet
 {
 	if ((Get-Command "dotnet" -ErrorAction SilentlyContinue) -eq $null) 
@@ -213,72 +164,18 @@ if ($command -eq "all" -or $command -eq "clean")
 	$bannerlordExe = $bannerlorddir + "/bin/Win64_Shipping_Client/bannerlord.exe"
 	$bannerlordModName = $env:MOD_NAME
 
-	#$templateDir = $pwd.Path
-	#$versionFile = $env:ENGINE_DIRECTORY + "/VERSION"
-	#$currentEngine = ""
+	echo "Checking Bannerlord Game....."
 
-	#if ($currentEngine -ne "" -and $currentEngine -eq $env:ENGINE_VERSION)
-	#{
-	#	cd $env:ENGINE_DIRECTORY
-	#	Invoke-Expression ".\make.cmd $command"
-	#	echo ""
-	#	cd $templateDir
-	#}
-	##elseif ($env:AUTOMATIC_ENGINE_MANAGEMENT -ne "True")
-	#{
-	#	echo "Automatic engine management is disabled."
-	#	echo "Please manually update the engine to version $env:ENGINE_VERSION."
-	#	WaitForInput
-	#}
-	#else
-	#{
-		echo "Checking Bannerlord Game....."
-
-		if (Test-Path $bannerlordExe)
-		{
-			Copy-Bannerlord-Dlls
-			
-			All-Command
-		}
-		else
-		{
-			echo "Bannerlord Game can't be founded"
-		}
-
-		#echo "Downloading engine..."
-		#
-		#if (Test-Path $env:AUTOMATIC_ENGINE_EXTRACT_DIRECTORY)
-		#{
-		#	rm $env:AUTOMATIC_ENGINE_EXTRACT_DIRECTORY -r
-		#}
-		#
-		#$url = $env:AUTOMATIC_ENGINE_SOURCE
-		#$url = $url.Replace("$", "").Replace("{ENGINE_VERSION}", $env:ENGINE_VERSION)
-		#
-		#mkdir $env:AUTOMATIC_ENGINE_EXTRACT_DIRECTORY > $null
-		#$dlPath = Join-Path $pwd (Split-Path -leaf $env:AUTOMATIC_ENGINE_EXTRACT_DIRECTORY)
-		#$dlPath = Join-Path $dlPath (Split-Path -leaf $env:AUTOMATIC_ENGINE_TEMP_ARCHIVE_NAME)
-		#
-		#$client = new-object System.Net.WebClient
-		#[Net.ServicePointManager]::SecurityProtocol = 'Tls12'
-		#$client.DownloadFile($url, $dlPath)
-		#
-		#Add-Type -assembly "system.io.compression.filesystem"
-		#[io.compression.zipfile]::ExtractToDirectory($dlPath, $env:AUTOMATIC_ENGINE_EXTRACT_DIRECTORY)
-		#rm $dlPath
-		#
-		#$extractedDir = Get-ChildItem $env:AUTOMATIC_ENGINE_EXTRACT_DIRECTORY -Recurse | ?{ $_.PSIsContainer } | Select-Object -First 1
-		#Move-Item $extractedDir.FullName -Destination $templateDir
-		#Rename-Item $extractedDir.Name (Split-Path -leaf $env:ENGINE_DIRECTORY)
-		#
-		#rm $env:AUTOMATIC_ENGINE_EXTRACT_DIRECTORY -r
-		#
-		#cd $env:ENGINE_DIRECTORY
-		#Invoke-Expression ".\make.cmd version $env:ENGINE_VERSION"
-		#Invoke-Expression ".\make.cmd $command"
-		#echo ""
-		#cd $templateDir
-	#}
+	if (Test-Path $bannerlordExe)
+	{
+		Copy-Bannerlord-Dlls
+		
+		All-Command
+	}
+	else
+	{
+		Write-Host "Bannerlord Game can't be founded" -ForegroundColor Green
+	}
 }
 
 $utilityPath = $env:ENGINE_DIRECTORY + "/OpenRA.Utility.exe"
